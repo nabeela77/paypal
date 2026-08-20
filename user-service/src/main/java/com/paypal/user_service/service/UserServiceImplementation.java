a7df838cd4d200c20fc1269fad29f433db114453
nabeela77 <149621875+nabeela77@users.noreply.github.com>
2026-08-20 15:04:28 -0700

    Created repository and service

---
+++ user-service/src/main/java/com/paypal/user_service/service/UserServiceImplementation.java
@@ -0,0 +1,32 @@
+package com.paypal.user_service.service;
+
+import com.paypal.user_service.entity.User;
+import com.paypal.user_service.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImplementation implements UserService {
+    private UserRepository userRepository;
+
+    public UserServiceImplementation(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createUser(User user) {
+        return (User) userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
