f3ef54632223a0455791eff1ebd547c9b22d7319
nabeela77 <149621875+nabeela77@users.noreply.github.com>
2026-08-20 16:54:08 -0700

    Created repository and service

--- user-service/src/main/java/com/paypal/user_service/controller/UserController.java
+++ user-service/src/main/java/com/paypal/user_service/controller/UserController.java
@@ -4,10 +4,9 @@ import com.paypal.user_service.entity.User;
 import com.paypal.user_service.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users/")
@@ -19,5 +18,15 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
 return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
+    }
+    @GetMapping("{/id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        return userService.getUserById((id)).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 }
